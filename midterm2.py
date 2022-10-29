497df1eaaa614d6c24b5edd425f1a8a097740bb6
Jared <jagunlimited81@gmail.com>
2022-10-29 18:30:30 -0500

    completed code

--- midterm2.py
+++ midterm2.py
@@ -9,20 +9,22 @@ class MVBEM:
     def __init__(self, train, test):
         # split data into words
         self.train_data = [(a, b.split(" ")) for a, b in train]
-        print(self.train_data[0])
+        # print(self.train_data[0])
         self.test_data = [(a, b.split(" ")) for a, b in test]
-        self.vocabulary = []
 
         # split into labels
-        self.train_data_ham = [_ for label, _ in self.train_data if label == "ham"]
-        self.train_data_spam = [_ for label, _ in self.train_data if label == "spam"]
+        self.train_data_ham = [_ for label,
+                               _ in self.train_data if label == "ham"]
+        self.train_data_spam = [_ for label,
+                                _ in self.train_data if label == "spam"]
 
-        print(self.train_data_spam[0])
-
-        self.len_ham = len(self.train_data_ham)
-        self.len_spam = len(self.train_data_spam)
+        # print(self.train_data_spam[0])
+        # get the number of total words
+        self.len_ham = sum([len(l) for l in self.train_data_ham])
+        self.len_spam = sum([len(l) for l in self.train_data_spam])
 
         # Create Vocabulary
+        self.vocabulary = []
         for label, data in self.test_data:
             for word in data:
                 self.vocabulary.append(word)
@@ -32,42 +34,55 @@ class MVBEM:
         self.wordCountsHam = {}
         self.wordCountsSpam = {}
         for word in self.vocabulary:
-            self.wordCountsHam[word] = sum([1 for i in self.wordCountsHam if word in i])
-            self.wordCountsSpam[word] = sum([1 for i in self.wordCountsSpam if word in i])
+            self.wordCountsHam[word] = sum(
+                [1 for i in self.train_data_ham if word in i])
+            self.wordCountsSpam[word] = sum(
+                [1 for i in self.train_data_spam if word in i])
+
         # Initiate parameters
-        self.parameters_spam = {unique_word:0 for unique_word in self.vocabulary}
-        self.parameters_ham = {unique_word:0 for unique_word in self.vocabulary}
+        self.parameters_spam = {
+            unique_word: 0 for unique_word in self.vocabulary}
+        self.parameters_ham = {
+            unique_word: 0 for unique_word in self.vocabulary}
 
         alpha = 1
 
         # Calculate parameters
         for word in self.vocabulary:
-            # sum up all occurences of the word 
+            # sum up all occurences of the word
 
-            self.n_word_given_spam = self.wordCountsSpam[word] # spam_messages already defined
-            self.p_word_given_spam = (self.n_word_given_spam + alpha) / (self.len_spam + alpha*self.len_vocabulary)
+            # spam_messages already defined
+            self.n_word_given_spam = self.wordCountsSpam[word]
+            self.p_word_given_spam = (
+                self.n_word_given_spam + alpha) / (self.len_spam + alpha*self.len_vocabulary)
             self.parameters_spam[word] = self.p_word_given_spam
 
-            self.n_word_given_ham = self.wordCountsHam[word] # ham_messages already defined
-            self.p_word_given_ham = (self.n_word_given_ham + alpha) / (self.len_ham + alpha*self.len_vocabulary)
+            # ham_messages already defined
+            self.n_word_given_ham = self.wordCountsHam[word]
+            self.p_word_given_ham = (
+                self.n_word_given_ham + alpha) / (self.len_ham + alpha*self.len_vocabulary)
             self.parameters_ham[word] = self.p_word_given_ham
 
     def hamOrSpam(self, email: str) -> str:
         email = email.split(' ')
-        # https://www.kdnuggets.com/2020/07/spam-filter-python-naive-bayes-scratch.html#:~:text=Classifying%20A%20New%20Message 
+
         p_ham = .5
         p_spam = .5
-
         for word in email:
             if word in self.parameters_spam:
-                print("here")
                 p_spam *= self.parameters_spam[word]
             if word in self.parameters_ham:
                 p_ham *= self.parameters_ham[word]
-        
-        print(p_ham)
-        print(p_spam)
-        return "ham"
+            while p_spam < 1/100 and p_ham < 1/100:
+                p_spam = p_spam * 10
+                p_ham = p_ham * 10
+        if p_ham > p_spam:
+            return "ham"
+        elif p_ham < p_spam:
+            return "spam"
+        else:
+            print("There was a tie")
+            return "tie"
 
 
 def load_datasets():
@@ -141,8 +156,46 @@ def main():
 
     print("\ncreating model...")
     model = MVBEM(train, test)
-    
-    model.hamOrSpam("")
+
+    print("\ntesting model...")
+    true_positive = 0
+    true_negative = 0
+    false_positive = 0
+    false_negative = 0
+    for answer, email in test:
+        model_answer = model.hamOrSpam(email)
+
+        # If we got the correct answer, TRUE answers only
+        if model_answer == answer:
+            if model_answer == "spam":
+                true_positive += 1
+            elif model_answer == "ham":
+                true_negative += 1
+        
+        else:
+            if model_answer == "spam":
+                false_positive += 1
+            elif model_answer == "ham":
+                false_negative += 1
+
+    print("\nResults:")
+    print(f"  true positives: {true_positive}")
+    print(f"  false negatives: {false_negative}")
+    print(f"  true negatives: {true_negative}")
+    print(f"  false positives: {false_positive}")
+    print()
+
+    precision = true_positive/(true_positive + false_positive)
+    recall = true_positive/(true_positive + false_negative)
+    f1 = 2 * ((precision * recall) / (precision + recall))
+    accuracy = (true_positive + true_negative) / len(test)
+
+    print("Statistics:")
+    print(f"  Precision: {precision}")
+    print(f"  Recall: {recall}")
+    print(f"  F1: {f1}")
+    print(f"  Accuracy: {accuracy}")
+    print()
 
 
 if __name__ == "__main__":
